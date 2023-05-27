415b9b763334d7ae1650531cd59569930b384660
kkilme <jojo5142@naver.com>
2023-05-28 07:20:05 +0900

    more uitext, singleton fix

--- Singleton.py
+++ Singleton.py
@@ -1,4 +1,4 @@
-class SingletonInstance:
+class SingletonInstance: # 이 클래스를 상속받으면 싱글톤으로 구현가능
     __instance = None
 
     @classmethod
@@ -6,7 +6,8 @@ class SingletonInstance:
         return cls.__instance
 
     @classmethod
-    def instance(cls, *args, **kargs):
-        cls.__instance = cls(*args, **kargs)
+    def instance(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = cls(*args, **kwargs)
         cls.instance = cls.__getInstance
         return cls.__instance
