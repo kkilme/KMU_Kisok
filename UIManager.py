1220f0c50049f8eb02a7243151a0078ea7031cf9
Chaewon14 <rabbit7653@naver.com>
2023-05-27 15:13:11 +0900

    UIManager selectNum if문

--- UIManager.py
+++ UIManager.py
@@ -1,4 +1,7 @@
 from uitexts import *
+from MenuManager import MenuManager
+from KioskHelper import *
+from AdminManager import *
 
 class UIManager:
     def __init__(self) -> None:
@@ -15,5 +18,16 @@ class UIManager:
             self.UIDict = UI_KR
 
     def MainScreen(self):
-        print(self.UIDict['mainscreen'])
-        n = input()
+        print(self.UIDict['mainscreen'])   #왜 오류나지
+
+        selectNum = input()
+        if selectNum == 1:  #메뉴 보기
+            MenuManager()
+        elif selectNum == 2:  #언어 설정
+            KioskHelper.SetKioskLanguage()
+        elif selectNum == 3:  #직원 호출
+            KioskHelper.CallEmployee()
+        elif selectNum == 4:  #관리자 모드
+            AdminManager()
+        else :
+            print("Error! Again Please")
