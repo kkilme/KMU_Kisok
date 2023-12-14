87133d04886b5c8e5220ac706363dbb951cb40c1
kkilme <jojo5142@naver.com>
2023-12-15 01:03:35 +0900

    remove unused files

--- Order.py
+++
@@ -1,8 +0,0 @@
-class Order:
-    def __init__(self, id, date, items, tprice, istakeout) -> None:
-        self.orderDate = date
-        self.orderId = id
-        self.orderItems = items # 주문한 메뉴 내역, 형식: {'menu1': 2}, 2=구매 수량(quantity)
-        self.totalPrice = tprice
-        self.isTakeOut = istakeout
-        
